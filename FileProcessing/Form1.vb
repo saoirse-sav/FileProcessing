497e9b6f822eac23bf8116b09fff73c88a93ea4c
saoirse-sav <hed-jteel@smu.edu.ph>
2025-11-04 19:04:06 +0800

    loop

--- FileProcessing/Form1.vb
+++ FileProcessing/Form1.vb
@@ -1,6 +1,8 @@
 ﻿Imports System.IO
 
 Public Class Form1
+
+    Dim filePath As String = "sample.txt"
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Try
             Dim filePath As String = "sample.txt"
@@ -24,6 +26,17 @@ Public Class Form1
         Using Reader As New StreamReader(filePath)
             Dim content As String = Reader.ReadToEnd()
             MessageBox.Show(content, "File Content: ")
+        End Using
+    End Sub
+
+    Private Sub Readmeperline_Click(sender As Object, e As EventArgs) Handles Readmeperline.Click
+        Using reader As New StreamReader(filePath)
+            Dim line As String = reader.ReadLine()
+            While (line IsNot Nothing)
+                ListBox1.Items.Add(line)
+                line = reader.ReadLine()
+            End While
+            'MessageBox.Show(line)
         End Using
     End Sub
 End Class
