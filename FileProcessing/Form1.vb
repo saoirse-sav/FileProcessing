2aab4647491af838768c874624c7d83697bfdf77
saoirse-sav <hed-jteel@smu.edu.ph>
2025-11-04 18:32:08 +0800

    Git commit demo

--- FileProcessing/Form1.vb
+++ FileProcessing/Form1.vb
@@ -1,3 +1,20 @@
-﻿Public Class Form1
+﻿Imports System.IO
 
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            Dim filePath As String = "sample.txt"
+
+            Using writer As New StreamWriter(filePath)
+                writer.WriteLine("Hello, this is a test file!")
+                writer.WriteLine("Second line of text.")
+            End Using
+
+        Catch ex As Exception
+            MessageBox.Show("Error: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End Try
+
+
+        MessageBox.Show("Data written successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
 End Class
